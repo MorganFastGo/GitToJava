23d347c0c183d40248c65cf9b534d8ad7b68666b
MorganFastGo <1290649785@qq.com>
2019-05-12 19:14:36 +0800

    这是测试项目

---
+++ git-test-LoadandUp/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,10 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("1111111111");
+        System.out.println("1111111111");
+        System.out.println("1111111111");
+        System.out.println("1111111111");
+    }
+}
